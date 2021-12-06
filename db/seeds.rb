a07d8a7573ba9b9cf0a4f0372b399647ced257b4
jiaen zhang <jiaen.zhang98@gmail.com>
2021-12-07 00:24:22 +0800

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,30 +3,27 @@ User.destroy_all
 Project.destroy_all
 Deliverable.destroy_all
 
-puts "Creating brands"
+puts "Creating main user"
 
-random_brands =
+User.create!({
+  email: "testuser@gmail.com",
+  password: "123456",
+  username: "testuser",
+  first_name: "Test",
+  last_name: "User",
+  account_type: "Creator",
+  profile_pic: Faker::Company.logo
+  })
 
-10.times do
-  User.create!({
-    email: Faker::Internet.email,
-    password: "123456",
-    username: Faker::Commerce.brand + Faker::Verb.base,
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    account_type: "brand",
-    profile_pic: Faker::Company.logo
-    })
-
-  puts "Creating projects for #{User.last.username}"
-  2.times do
-    Project.create!({
-      user: User.last,
-      name: Faker::Marketing.buzzwords,
-      project_end: Faker::Date.between(from: '2021-12-12', to: '2022-02-12'),
-      status: ['Pending', 'Saved', 'Ongoing', 'Completed'].sample,
-      description: Faker::Company.bs
-    })
+20.times do
+  Project.create!({
+    user: User.last,
+    name: Faker::Marketing.buzzwords,
+    project_end: Faker::Date.between(from: '2021-12-12', to: '2022-02-12'),
+    status: ['Pending', 'Saved', 'Ongoing', 'Completed'].sample,
+    description: Faker::Company.bs,
+    brand: Faker::Verb.base.capitalize + Faker::Commerce.brand
+  })
 
     puts "Creating deliverables for #{Project.last.name}"
     3.times do
@@ -37,7 +34,7 @@ random_brands =
         description: Faker::Company.bs
       })
     end
-  end
 end
+
 
 puts "All done!"
