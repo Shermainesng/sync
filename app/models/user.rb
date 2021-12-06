232722aca6bc836ee281f47c88f370d623561e9c
Huiling Loh <huiling_loh_97@hotmail.com>
2021-12-06 15:13:54 +0800

    added relations between users and projects

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :projects, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true
+  validates :type, presence: true
 end
