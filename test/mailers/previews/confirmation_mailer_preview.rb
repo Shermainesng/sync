ab6b078dd84d44ddc953e2caa3ca965d1a1bd26b
Huiling Loh <huiling_loh_97@hotmail.com>
2021-12-07 18:09:07 +0800

    created email popup

--- test/mailers/previews/confirmation_mailer_preview.rb
+++ test/mailers/previews/confirmation_mailer_preview.rb
@@ -3,11 +3,7 @@ class ConfirmationMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
   def send_project
-    user = User.first
-    ConfirmationMailer.with(client_email: 'zzz.gmail.com',
-                            email_subject: 'teset',
-                            email_body: 'testing',
-                            user: user).send_project
+    ConfirmationMailer.send_project
   end
 
 end
