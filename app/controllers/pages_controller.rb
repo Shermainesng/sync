86a7f65d491024c4d2a3dc16631c73f37b93009e
Shermaine Sng <shersmallsng@gmail.com>
2021-12-08 18:51:34 +0800

    search function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @projects = current_user.projects
+    @projects = current_user.projects.where(status: "Ongoing")
     @deliverables = current_user.deliverables.order(:due_date)
   end
 end
--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -4,13 +4,6 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ["dropdown", "list", "test", "projects", "Ongoing", "Pending", "Saved", "Completed", "All"]
 
-  connect() {
-    console.log("filter here");
-    // console.log(`dropwdown target: ${this.dropdownTarget}`);
-    // console.log(`list target: ${this.listTarget}`);
-    // console.log(`test target: ${this.testTarget}`);
-  }
-
   filterTasks() {
     this.listTarget.innerHTML = "";
 
