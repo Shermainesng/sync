da73149fb7af26d9c16ba8766d3f46bf08fd2ebb
Shermaine Sng <shersmallsng@gmail.com>
2021-12-08 14:26:32 +0800

    added delete deliverable

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -1,6 +1,6 @@
 class Draft < ApplicationRecord
   mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
   belongs_to :deliverable
-  has_many :comments
+  has_many :comments, dependent: :destroy
   belongs_to :user
 end
