0e4414e349c077faf0d40bddf044115343ef126b
jiaen zhang <jiaen.zhang98@gmail.com>
2021-12-09 09:09:54 +0800

    fixed layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,11 @@ class PagesController < ApplicationController
 
     date = Date.today + 7
     @deliverables = current_user.deliverables.where("due_date <= ?", date).order(:due_date)
-    @ongoing = current_user.projects.where(status: "Ongoing")
-    @pending = current_user.projects.where(status: "Pending")
+    @all = current_user.projects
+    @ongoing = @all.where(status: "ongoing")
+    @pending = @all.where(status: "pending")
+    @completed = @all.where(status: "completed")
+    @saved = @all.where(status: "saved")
 
-  end
-
-  def test
   end
 end
--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -2,46 +2,45 @@ import { csrfToken } from "@rails/ujs";
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["dropdown", "list", "projects", "status"]
+  static targets = ["dropdown", "deliverables", "projects", "input"]
 
+  connect () {
+    console.log("filter connected");
+  }
 
   filterTasks() {
-    this.listTarget.innerHTML = "";
+    this.deliverablesTarget.innerHTML = "";
 
     fetch(`deliverables?deliv_by=${this.dropdownTarget.value}`, {
       headers: { 'Accept': "text/plain" }
     })
       .then((res) => res.text())
       .then((data)=> {
-        this.listTarget.innerHTML= data;
+        this.deliverablesTarget.innerHTML= data;
       });
   }
 
-  filterProjects(event) {
-    this.projectsTarget.innerHTML="";
-    debugger
-    const status =  event.currentTarget.value
-    const name = this.inputTarget.value
 
-    fetch(`projects?filter_status=${status}&filter_name=${name}`, {
+  initialize() {
+    this.timer
+  }
+
+  filterProjects() {
+    clearTimeout(this.timer);
+
+    this.timer = setTimeout(() => {
+      debugger
+      this.projectsTarget.innerHTML = "";
+
+      fetch(`projects?&filter_name=${this.inputTarget.value}`, {
         headers: { 'Accept': "text/plain" }
       })
         .then((res) => res.text())
         .then((data) => {
-          // console.log(data);
           this.projectsTarget.innerHTML = data;
         });
+
+    }, 500);
+
     }
 }
-
-// update() {
-//   this.listTarget.innerHTML = "";
-//   const url = `projects/?query=${this.inputTarget.value}`
-//   fetch(url, {
-//     headers: { 'Accept': 'text/plain' }
-//   })
-//     .then(response => response.text())
-//     .then((data) => {
-//       console.log(data);
-//     });
-// }
