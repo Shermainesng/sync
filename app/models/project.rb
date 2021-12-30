f8b6e2da911605a27ed000b8a72d2cc153fe6877
jiaen zhang <jiaen.zhang98@gmail.com>
2021-12-30 10:18:52 +0800

    added has_and_belong_to_many association

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,7 @@ class Project < ApplicationRecord
   # has_many :users, through: :project_users, as: :clients
   has_many :deliverables, dependent: :destroy
   has_many :drafts, through: :deliverables
+  has_and_belongs_to_many :users, join_table: :project_users
 
   # scope :filter_status, ->status { where("status ILIKE ?", status) }
   scope :filter_name, ->name { where("name ILIKE ?", "%#{name}%")}
