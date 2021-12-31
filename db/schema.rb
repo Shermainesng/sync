13e863445991ef89f9099c25b4f72e16457aa963
Huiling Loh <huiling_loh_97@hotmail.com>
2021-12-31 11:11:54 +0800

    remove account type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_30_014923) do
+ActiveRecord::Schema.define(version: 2021_12_30_083634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -127,7 +127,6 @@ ActiveRecord::Schema.define(version: 2021_12_30_014923) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "account_type"
     t.string "first_name"
     t.string "last_name"
     t.string "username"
