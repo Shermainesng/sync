c796ae955a251f905d9e18c397c2a32fa5e9ebc1
jiaen zhang <jiaen.zhang98@gmail.com>
2022-01-12 15:51:00 +0800

    added react components for deliverables

--- app/javascript/components/deliverable.jsx
+++ app/javascript/components/deliverable.jsx
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 
 const Date = (props) => {
   return (
+    <div>
+      {moment(props.date).format("MMM D, ddd")}
+    </div>
+  );
+}
 
+const Description = (props) => {
+  return (
+    <div className="card-custom-title">
+      <p className="truncate">
+        {props.description}
+      </p>
+    </div>
   );
 }
 
@@ -10,7 +22,9 @@ const Deliverable = (props) => {
   return (
     <div className="card-white-and-date mx-auto">
       <Date date={props.date}/>
-
+      <div class="card-deliverable-content pt-1">
+        <Description description={props.description}/>
+      </div>
     </div>
   );
 };
