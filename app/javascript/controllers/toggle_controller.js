c9b1ff8444fe34c112d36978b4fe2c00a325e76d
Huiling Loh <huiling_loh_97@hotmail.com>
2022-01-20 23:39:08 +0800

    show form to send email in modal

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,7 +2,7 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["commentForm", "button"];
+  static targets = ["commentForm", "button", "emailBodyField"];
 
   connect() {
     console.log("hello from toggle controller!");
@@ -11,6 +11,14 @@ export default class extends Controller {
   newComment() {
     this.commentFormTarget.classList.toggle("d-none");
     this.buttonTarget.classList.toggle("d-none");
+  }
+
+  show() {
+    this.emailBodyFieldTarget.classList.remove("d-none")
+  }
+
+  hide() {
+    this.emailBodyFieldTarget.classList.add("d-none")
   }
 
 }
