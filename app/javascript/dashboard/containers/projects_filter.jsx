a334fa4aa4389a39cbfcad03f33b1463d43d1739
jiaen zhang <jiaen.zhang98@gmail.com>
2022-01-21 11:25:09 +0800

    added projects filter functionality in react

--- app/javascript/dashboard/containers/projects_filter.jsx
+++ app/javascript/dashboard/containers/projects_filter.jsx
@@ -1,32 +1,34 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { loadProjects } from '../actions';
+
 
 
 class ProjectsFilter extends Component {
 
-  // handleChange(e) {
-  //   this.props.loadDeliverables(e.target.value);
-  // }
+  handleClick(e) {
+    this.props.loadProjects({ status: e.target.attributes.value.value, name: "" });
+  }
 
   render() {
     return (
       <div className="d-flex justify-content-between align-items-center mb-4">
         <ul className="filter-bar nav nav-tabs" id="myTab" role="tablist">
           <li role="presentation">
-            <a className="nav-link active" data-toggle="tab" value="ongoing">Ongoing</a>
+            <a className="nav-link active" data-toggle="tab" value="ongoing" onClick={(e) => this.handleClick(e)}>Ongoing</a>
           </li>
           <li role="presentation">
-            <a className="nav-link" data-toggle="tab" value="pending">Pending</a>
+            <a className="nav-link" data-toggle="tab" value="pending" onClick={(e) => this.handleClick(e)}>Pending</a>
           </li>
           <li role="presentation">
-            <a className="nav-link" data-toggle="tab" value="completed">Completed</a>
+            <a className="nav-link" data-toggle="tab" value="completed" onClick={(e) => this.handleClick(e)}>Completed</a>
           </li>
           <li role="presentation">
-            <a className="nav-link" data-toggle="tab" value="saved">Saved</a>
+            <a className="nav-link" data-toggle="tab" value="saved" onClick={(e) => this.handleClick(e)}>Saved</a>
           </li>
           <li role="presentation">
-            <a className="nav-link" data-toggle="tab" value="all">All</a>
+            <a className="nav-link" data-toggle="tab" value="" onClick={(e) => this.handleClick(e)}>All</a>
           </li>
         </ul>
       </div>
@@ -34,17 +36,16 @@ class ProjectsFilter extends Component {
   }
 }
 
-// function mapDispatchToProps(dispatch) {
-//   return bindActionCreators(
-//     { loadDeliverables: loadDeliverables },
-//     dispatch);
-// }
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { loadProjects: loadProjects },
+    dispatch);
+}
 
-// function mapStateToProps(state) {
-//   return {
-//     deliverables: state.deliverables
-//   };
-// }
+function mapStateToProps(state) {
+  return {
+    projects: state.projects
+  };
+}
 
-// export default connect(mapStateToProps, mapDispatchToProps)(ProjectsFilter);
-export default ProjectsFilter;
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectsFilter);
