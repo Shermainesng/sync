9dc0841e41df9fbd0b6428100fdd62c98dbd7496
Shermainesng <shersmallsng@gmail.com>
2022-02-12 23:27:20 +0800

    new changes for react

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
   # patch "/deliverables/:id", to: "deliverables#update" , as: 'update_deliverable'
 
   get '/test', to: 'pages#test'
-  get '/dashboard', to: 'pages#dashboard'
-  get '/projects/:id/edit', to:'pages#dashboard'
+
+  # get '/dashboard', to: 'pages#dashboard'
+  # get '/projects/:id/edit', to:'pages#dashboard'
   get '/error', to: 'pages#error'
 
   get '/notifications', to: 'notifications#index'
@@ -48,8 +49,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :deliverables, only: [:index]
-      resources :projects, only: [:index, :show] do
-        get 'deliverables', to: "projects#deliverables"
+      resources :projects, only: %i[index show] do
+        get 'deliverables', to: 'projects#deliverables'
       end
     end
   end
