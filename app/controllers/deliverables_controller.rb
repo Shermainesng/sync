6851270d8e8103233188357608b7a9ba9dc16469
Shermainesng <shersmallsng@gmail.com>
2022-02-13 16:43:08 +0800

    bundle

--- app/controllers/deliverables_controller.rb
+++ app/controllers/deliverables_controller.rb
@@ -1,24 +1,24 @@
 class DeliverablesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-  before_action :set_deliverable, only: [:show, :destroy, :edit, :update]
+  before_action :set_deliverable, only: %i[show destroy edit update]
 
   def index
     filter = params[:deliv_by] #week or month
 
     case filter
-      when "week"
-        filtered_date = Date.today + 7
-      when "two_weeks"
-        filtered_date = Date.today + 14
-      when "month"
-        filtered_date = Date.today + 1.month
+    when 'week'
+      filtered_date = Date.today + 7
+    when 'two_weeks'
+      filtered_date = Date.today + 14
+    when 'month'
+      filtered_date = Date.today + 1.month
     end
 
-    @deliverables= current_user.deliverables.where("due_date >= ?", Date.today).where("due_date <= ?", filtered_date).order(:due_date)
+    @deliverables = current_user.deliverables.where('due_date >= ?', Date.today).where('due_date <= ?', filtered_date).order(:due_date)
 
     respond_to do |format|
-      format.html { redirect_to root_path}
+      format.html { redirect_to root_path }
       format.text { render partial: 'deliverables/cards/dashboard', collection: @deliverables, as: :deliverable, formats: [:html] }
     end
   end
@@ -49,7 +49,6 @@ class DeliverablesController < ApplicationController
       format.html { redirect_to edit_project_path(@project) }
       format.json # Follow the classic Rails flow and look for a create.json view
       format.text { render partial: 'deliverables/cards/project_details', collection: @deliverables, as: :deliverable, formats: [:html] }
-
     end
   end
 
@@ -58,15 +57,16 @@ class DeliverablesController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to edit_project_path(@project) }
+
       # format.json # Follow the classic Rails flow and look for a create.json view
-      format.text { render plain: "ok" }
+      format.text { render plain: 'ok' }
     end
   end
 
   def edit
     respond_to do |format|
       format.html { redirect_to edit_project_path(@project) }
-      format.text {render partial: 'deliverables/update', locals: { deliverable: @deliverable }, formats: [:html]}
+      format.text { render partial: 'deliverables/update', locals: { deliverable: @deliverable }, formats: [:html] }
     end
   end
 
@@ -74,22 +74,18 @@ class DeliverablesController < ApplicationController
     @project = Project.find(@deliverable[:project_id])
 
     updated = params[:deliverable]
-    @deliverable.update!({
-      deliverable_type: updated[:deliverable_type],
-      due_date: "#{updated["due_date(1i)"]}-#{updated["due_date(2i)"]}-#{updated["due_date(3i)"]}",
-      description: updated[:description],
-      tag_list: updated[:tag_list].split.join(",")
-    })
+    @deliverable.update!({ deliverable_type: updated[:deliverable_type], due_date: "#{updated['due_date(1i)']}-#{updated['due_date(2i)']}-#{updated['due_date(3i)']}", description: updated[:description], tag_list: updated[:tag_list].split.join(',') })
 
     respond_to do |format|
-        # format.html { redirect_to edit_project_path(@project) }
-        format.json # Follow the classic Rails flow and look for a create.json view
-        format.text {render partial: 'deliverables/cards/project_details', locals: { deliverable: @deliverable }, formats: [:html]}
-        # format.text {render plain: 'ok' }
+      # format.html { redirect_to edit_project_path(@project) }
+      format.json # Follow the classic Rails flow and look for a create.json view
+      format.text { render partial: 'deliverables/cards/project_details', locals: { deliverable: @deliverable }, formats: [:html] }
+      # format.text {render plain: 'ok' }
     end
   end
 
   private
+
   def deliverable_params
     params.require(:deliverable).permit(:deliverable_type, :description, :tag_list)
   end
@@ -101,7 +97,6 @@ class DeliverablesController < ApplicationController
   end
 
   def record_not_found
-    render "pages/error", status: 404
+    render 'pages/error', status: 404
   end
-
 end
