fd27d602604190d48e7188a8f18c4b28cd27a370
jiaen zhang <jiaen.zhang98@gmail.com>
2022-03-10 00:40:27 +0800

    fixed conflicts

--- app/javascript/dashboard/containers/show_project.js
+++ app/javascript/dashboard/containers/show_project.js
@@ -3,13 +3,8 @@ import { Link, Outlet } from "react-router-dom";
 // import { bindActionCreators } from 'redux';
 // import { connect } from 'react-redux';
 
-<<<<<<< HEAD
-// import DeliverablesList from './deliverables_list';
-import FrostedCard from "../components/frostedcard";
-=======
 import DeliverablesList from '../components/deliverables_list';
 import FrostedCard from '../components/frostedcard';
->>>>>>> 34e3a2c384aceb6a4dfa0a1b9a722bdde26fa248
 
 class ShowProject extends Component {
   componentDidMount() {}
